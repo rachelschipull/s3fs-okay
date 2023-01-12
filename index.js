63c2e39ea26a214a8279f551e3d78d39064000d8
Mike <korostelevm@gmail.com>
2023-01-12 13:17:55 -0500

    added a route t

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const fs = require('fs')
+
+ 
+
+app.get('/', async (req, res) => {
+    fs.writeFileSync('my_file.txt', new Date().toISOString())
+     
+    return res.send('Hello World!');
+});
+app.get('/contents', async (req, res) => {
+    console.log('/contents route')
+    let content = fs.readFileSync('my_file.txt').toString()
+     
+    return res.send(content);
 });
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
-});
+app.listen(3000, () => {});
