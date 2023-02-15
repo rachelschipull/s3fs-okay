8a895e802fcb5bff8874fe3ce247b1f26e900737
Rachel Schipull <rlschipull@gmail.com>
2023-02-15 11:19:20 -0500

    add aws s3 sdk

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const express = require('express');
 const app = express();
 
 const fs = require('@cyclic.sh/s3fs')(process.env.CYCLIC_BUCKET_NAME)
- 
- 
+
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3()
 
 app.get('/', async (req, res) => {
     fs.writeFileSync('my_file.txt', new Date().toISOString())
-     
     return res.send('Hello World!');
 });
+
 app.get('/contents', async (req, res) => {
     console.log('/contents route')
     let content = fs.readFileSync('my_file.txt').toString()
